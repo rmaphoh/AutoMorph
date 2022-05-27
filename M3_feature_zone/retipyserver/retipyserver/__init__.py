e91d1b0403558dacac112ba05700036a260b38a2
arajesh17 <anandwa@gmail.com>
2022-05-26 20:36:14 -0700

    deleted files

--- M3_feature_zone/retipyserver/retipyserver/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-base_url = "/retipy/"
-from . import endpoint_main
-from . import endpoint_main
-from . import endpoint_tortuosity
-from . import endpoint_segmentation
-from . import endpoint_landmarks
-from . import endpoint_vessel_classification
